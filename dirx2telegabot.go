2f5f2d022e0981243ad7b14153d40cfdbc19af17
pas <pchupail@rambler.ru>
2023-04-16 21:32:53 +0500

    Оповещения DirectumRX в Telegram чатбот

--- dirx2telegabot.go
+++ dirx2telegabot.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 var (
-	crtFile = filepath.Join(".", "certs", "client.crt")
-	keyFile = filepath.Join(".", "certs", "client.key")
+	crtFile = filepath.Join(".", "certs", "server.crt")
+	keyFile = filepath.Join(".", "certs", "server.key")
 )
 
 func main() {
@@ -19,67 +21,26 @@ func main() {
 	log.SetFlags(log.Lshortfile)
 
 	// TLS connect. Подключение по протоколу TLS
-	http.HandleFunc("/", handler)
-	log.Fatal(http.ListenAndServeTLS("localhost:8077", crtFile, keyFile, nil))
+	//mux := http.NewServeMux()
+	//mux.HandleFunc("/Заявка", handler)
+	//mux.HandleFunc("/Задание", handler)
+	http.HandleFunc("/Заявка", handler)
+	//log.Fatal(http.ListenAndServeTLS("localhost:8077", crtFile, keyFile, nil))
+	log.Fatal(http.ListenAndServe("localhost:8077", nil))
 }
 
 // This handler is returning component path of URL. Обработчик возвращает путь к компоненту URL
 func handler(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
-
-	////////////////////////////////////////////////////////////////////
-	// Parsing rows of data from a http-request
-	// Парсинг строк данных http-запроса
-	//sd := &DataType{}
-	counts := make(map[string]int)
-	//Slice for save all keys from mapping. Срез для хранения всех ключей мапы
-	datakeys := make([]string, 0, len(counts))
-	for _, line := range strings.Split(string(pars), ":") {
-		counts[line]++
-		log.Println(line) // Checks data of mapping. Проверка данных мапы
-	}
-	// Sorts keys to list and to sets values in order
-	// Сортировка ключей для перечисления и присваивания значений по порядку
-	for countkeys := range counts {
-		datakeys = append(datakeys, countkeys)
-	}
-	sort.Strings(datakeys)
-	for _, countkeys := range datakeys {
-		fmt.Printf("\nCountkeys: %v\nCounts: %v\n", countkeys, counts[countkeys])
-		if countkeys != "" {
-			if sp.weight == "" {
-				sp.mu.Lock()
-				sp.weight = countkeys
-				sp.mu.Unlock()
-			} else {
-				if sp.plan_default == "" {
-					sp.mu.Lock()
-					sp.plan_default = countkeys
-					sp.mu.Unlock()
-				} else {
-					sp.mu.Lock()
-					sp.value = countkeys
-					sp.mu.Unlock()
-				}
-			}
-		}
-	}
-	// Output for test. Тестовый вывод данных.
-	log.Println("Weight: ", sp.weight)
-	log.Println("Plan_default: ", sp.plan_default)
-	log.Println("Value : ", sp.value)
 
 	// Getting token from config. Получение токена из конфига.
 	chkey := make(chan string)
 	go func() {
 		chkey <- ReadBotKey()
 	}()
-	sbot := <-chkey
 
-	bot, err := tgbotapi.NewBotAPI(sbot)
+	bot, err := tgbotapi.NewBotAPI(<-chkey)
 	if err != nil {
-		//log.Panic(err)
 		log.Fatalf("Error of bot-api: %v", err)
 	}
 
@@ -98,6 +59,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 			msg.ReplyToMessageID = update.Message.MessageID
+			tmsg := "DirectumRX:Входящие:Задание"
+			msg.Text = (tmsg)
 
 			bot.Send(msg)
 		}
