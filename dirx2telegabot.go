f3fed4b3fc39e5155044572a9391f8d9c5cfaa62
pas <pchupail@rambler.ru>
2023-05-01 23:00:34 +0500

    http-gateway_choice-keyboard

--- dirx2telegabot.go
+++ dirx2telegabot.go
@@ -16,6 +16,19 @@ var (
 	keyFile = filepath.Join(".", "certs", "server.key")
 )
 
+var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
+		tgbotapi.NewInlineKeyboardButtonData("2", "2"),
+		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
+		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
+		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
+	),
+)
+
 func main() {
 	log.SetPrefix("Client event: ")
 	log.SetFlags(log.Lshortfile)
@@ -57,44 +70,38 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	updates := bot.GetUpdatesChan(u)
 
+	// Loop through each update.
 	for update := range updates {
-		if r.URL.Path == "" { //
-			continue
-		}
+		// Check if we've gotten a message update.
+		if update.Message != nil {
+			// Construct a new message from the given chat ID and containing
+			// the text that we received.
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
-		/*if update.Message == nil { // ignore any non-Message updates
-			continue
-		}
+			// If the message was open, add a copy of our numeric keyboard.
+			switch update.Message.Text {
+			case "open":
+				msg.ReplyMarkup = numericKeyboard
 
-		if !update.Message.IsCommand() { // ignore any non-command Messages
-			continue
-		}*/
+			}
 
-		// Create a new MessageConfig. We don't have text yet,
-		// so we leave it empty.
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+			// Send the message.
+			if _, err = bot.Send(msg); err != nil {
+				panic(err)
+			}
+		} else if update.CallbackQuery != nil {
+			// Respond to the callback query, telling Telegram to show the user
+			// a message with the data received.
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				panic(err)
+			}
 
-		// Extract the command from the Message.
-		switch r.URL.Path {
-		case "/Приказ":
-			update.Message.Command()
-			msg.Text = "Ознакомтесь с Приказом."
-			r.URL.Path = ""
-		case "/Заявка":
-			update.Message.Command()
-			msg.Text = "Вам поступила Заявка."
-			r.URL.Path = ""
-		case "/Задание":
-			update.Message.Command()
-			msg.Text = "Вам поступило Задание."
-			r.URL.Path = ""
-		default:
-			update.Message.Command()
-			msg.Text = "Поступил документ."
-		}
-
-		if _, err := bot.Send(msg); err != nil {
-			log.Panic(err)
+			// And finally, send a message containing the data received.
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+			if _, err := bot.Send(msg); err != nil {
+				panic(err)
+			}
 		}
 	}
 }
